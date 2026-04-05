668ad9dbcaa4959200cae82492648a98e5033bd5
Eccentric-jamaican <aellisatl@gmail.com>
2026-04-04 23:17:10 -0500

    Remove dist folder from git

--- dist/dev/server/middleware.js
+++
@@ -1,9 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/node_modules_next_headers_0bc0uj~.js")
-R.c("server/chunks/node_modules_next_0jeu.ne._.js")
-R.c("server/chunks/node_modules_@clerk_shared_dist_runtime_0fzct5a._.js")
-R.c("server/chunks/node_modules_@clerk_backend_dist_0ftt_a_._.js")
-R.c("server/chunks/node_modules_@clerk_nextjs_dist_esm_0oqjc11._.js")
-R.c("server/chunks/[root-of-the-server]__0cdxirb._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SKRR67QunXE67J7Tgg0e0CdocKXRaBWh8f3KIuloGwY=\"\n}"
